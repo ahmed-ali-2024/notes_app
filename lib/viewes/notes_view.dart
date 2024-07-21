e1a97bca0fcd7c8e2550a4f254aa82310789c63b
ahmed-ali-2024 <a3a1981@gmail.com>
2024-07-21 19:25:17 +0300

    custom appbar , search icon

---
+++ lib/viewes/notes_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/viewes/widgets/notes_view_body.dart';
+
+class NoteViews extends StatelessWidget {
+  const NoteViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
+  }
+}
---
+++ lib/viewes/widgets/notes_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/viewes/widgets/custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 16,
+          ),
+          customAppBar(),
+        ],
+      ),
+    );
+  }
+}
